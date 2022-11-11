dcade47ca7f5b2f19f726af67d080bfe689421c0
Dmitry <mrgames2014@yandex.ru>
2022-11-11 23:37:16 +0500

    Luna

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+function isValid($card){
+    $paymentSystem = ["4" => "VISA","5"=>"Mastercard ","220"=>"MIR"];
+
+    $card = str_replace(' ','',$card);
+    $arr = array();
+    for($i = 0; $i < strlen($card); $i++){
+        $arr[$i] = (int)($card[$i]);
+    }
+    for($j = 0; $j < count($arr); $j+=2){
+        $arr[$j]*= 2;
+        if($arr[$j] >= 10){
+            $arr[$j] = $arr[$j] % 10 + intdiv($arr[$j],$arr[$j]%10);
+        }
+    }
+    if(array_key_exists($card[0],$paymentSystem)){
+        $paymentSystem = $paymentSystem[$card[0]];
+    }
+    elseif(array_key_exists(substr($card,0,3),$paymentSystem)){
+        $paymentSystem = $paymentSystem[substr($card,0,3)];
+    }
+    else{
+        $paymentSystem = "UNKNOWN";
+    }
+    $isvalid = (array_sum($arr) % 10 == 0) ? "true" : "false";
+    return ["isValid" => $isvalid, "paymentSystem"=> $paymentSystem];
+
+}
+print_r(isValid("2201 1111 1111 1111"));
+
+?>
